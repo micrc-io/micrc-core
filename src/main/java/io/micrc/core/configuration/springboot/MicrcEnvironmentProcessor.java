aa9727bacbf98bb837543ad6fe150a385f2e3980
weiguan <weiguan@ttyys.io>
2022-09-07 06:07:30 +0800

    weiguan: 1. init spring-cloud-kubernetes support configmap 2. add sources jar

--- src/main/java/io/micrc/core/configuration/springboot/MicrcEnvironmentProcessor.java
+++ src/main/java/io/micrc/core/configuration/springboot/MicrcEnvironmentProcessor.java
@@ -1,6 +1,10 @@
 package io.micrc.core.configuration.springboot;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 import org.apache.commons.logging.Log;
 import org.springframework.boot.SpringApplication;
@@ -8,9 +12,13 @@ import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StringUtils;
 
-@Order(Ordered.LOWEST_PRECEDENCE)
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class MicrcEnvironmentProcessor implements EnvironmentPostProcessor {
 
     private final Log log;
@@ -22,8 +30,36 @@ public class MicrcEnvironmentProcessor implements EnvironmentPostProcessor {
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
         log.info("Processing Environment For Micrc...");
-        Properties properties = new Properties();
+        Collection<String> profiles = getProfiles(environment);
+        Properties properties = this.loadMicrcProperties();
+        properties.setProperty("application.profiles",
+            StringUtils.arrayToCommaDelimitedString(profiles.toArray(new String[]{})));
+        log.info("Micrc Application Profiles: " + profiles);
+        if (profiles.contains("default")) {
+            log.info("Local Default Environment, Disable Kubernetes Configmap Support. ");
+            properties.setProperty("spring.cloud.kubernetes.enabled", "false");
+        }
         PropertiesPropertySource source = new PropertiesPropertySource("micrc", properties);
         environment.getPropertySources().addLast(source);
+    }
+
+    private Properties loadMicrcProperties() {
+        Resource resource = new ClassPathResource("micrc.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(resource.getInputStream());
+        } catch (IOException e) {
+            throw new IllegalStateException("Unable loading properties from 'micrc.properties'", e);
+        }
+        return properties;
+    }
+
+    private Collection<String> getProfiles(Environment env) {
+        String activeProfilesConfig = env.getProperty("spring.profiles.active");
+        if (StringUtils.hasText(activeProfilesConfig)) {
+            return Arrays.asList(activeProfilesConfig.split(",")).stream()
+                    .map(String::trim).collect(Collectors.toSet());
+        }
+        return Arrays.asList(env.getDefaultProfiles());
     }
 }
--- src/main/java/io/micrc/core/persistence/springboot/PersistenceEnvironmentProcessor.java
+++ src/main/java/io/micrc/core/persistence/springboot/PersistenceEnvironmentProcessor.java
@@ -1,24 +1,19 @@
 package io.micrc.core.persistence.springboot;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 import java.util.Properties;
-import java.util.stream.Collectors;
 
 import org.apache.commons.logging.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.PropertiesPropertySource;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.util.StringUtils;
 
-@Order(Ordered.HIGHEST_PRECEDENCE)
 public class PersistenceEnvironmentProcessor implements EnvironmentPostProcessor {
 
     private final Log log;
@@ -29,38 +24,20 @@ public class PersistenceEnvironmentProcessor implements EnvironmentPostProcessor
 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-        Collection<String> profiles = getProfiles(environment);
-        Properties properties = this.loadMicrcProperties();
+        Optional<String> profileStr = Optional.ofNullable(environment.getProperty("application.profiles"));
+        List<String> profiles = Arrays.asList(profileStr.orElse("").split(","));
+        Properties properties = new Properties();
         envForH2Embedded(profiles, properties);
-        envForLiquibase(profiles, properties);
+        envForLiquibase(profiles, properties, environment);
         envForJPA(properties);
 
         PropertiesPropertySource source = new PropertiesPropertySource("micrc-persistence", properties);
         environment.getPropertySources().addLast(source);
     }
 
-    private Collection<String> getProfiles(Environment env) {
-        String activeProfilesConfig = env.getProperty("spring.profiles.active");
-        if (StringUtils.hasText(activeProfilesConfig)) {
-            return Arrays.asList(activeProfilesConfig.split(",")).stream()
-                    .map(String::trim).collect(Collectors.toSet());
-        }
-        return Arrays.asList(env.getDefaultProfiles());
-    }
-
-    private Properties loadMicrcProperties() {
-        Resource resource = new ClassPathResource("micrc.properties");
-        Properties properties = new Properties();
-        try {
-            properties.load(resource.getInputStream());
-        } catch (IOException e) {
-            log.error("Unable loading properties from 'micrc.properties'", e);
-        }
-        return properties;
-    }
-    
     private void envForH2Embedded(Collection<String> profiles, Properties properties) {
         if (profiles.contains("local") || profiles.contains("default")) { // 只有local和default环境使用h2
+            log.info("Start H2 Database For Environment Of 'local' And 'default'");
             properties.setProperty("spring.datasource.driver-class-name", "org.h2.Driver");
             properties.setProperty("spring.datasource.url", "jdbc:h2:mem:db;MODE=MYSQL");
             properties.setProperty("spring.datasource.username", "sa");
@@ -72,12 +49,12 @@ public class PersistenceEnvironmentProcessor implements EnvironmentPostProcessor
         }
     }
 
-    private void envForLiquibase(Collection<String> profiles, Properties properties) {
+    private void envForLiquibase(Collection<String> profiles, Properties properties, Environment env) {
         properties.setProperty("spring.liquibase.enabled", "false"); // 默认先关闭
         // 客户端系统应该在micrc.properties文件中定义application.version=${version}这个属性
         // 这个version会在gradle处理resources时通过project.properties进行expand，填充为当前build.gradle中的版本号
         // 这个版本号会带给liquibase使用，在使用内存库时，首选执行snapshot快照，然后执行版本号指定的changeset
-        String appVersion = properties.getProperty("application.version");
+        String appVersion = env.getProperty("application.version");
         if (!StringUtils.hasText(appVersion)) {
             throw new IllegalStateException(
                 "Unable find application version. "
@@ -86,6 +63,7 @@ public class PersistenceEnvironmentProcessor implements EnvironmentPostProcessor
         // 用于master include内存库的changelog的main文件
         String mem = "";
         if (profiles.contains("default") || profiles.contains("local")) {
+            log.info("Use Snapshot And Changeset For Memory Database. ");
             mem = "-mem";
         }
         // 两种情况下，liquibase在app内打开
@@ -93,13 +71,13 @@ public class PersistenceEnvironmentProcessor implements EnvironmentPostProcessor
         // 2. 当系统profile为default(本地开发)时，系统自动通过snapshot和当前版本changelog每次启动时初始化数据库
         // note: local(本地集群)环境，也是使用dbinit独立image在init container中初始化数据库
         if (profiles.contains("dbinit") || profiles.contains("default")) {
-            log.debug("Active Profile: dbinit\nEnable liquibase for init of db");
+            log.info("Active Profile: 'dbinit' Or 'default'. Enable liquibase For Database Init.");
             properties.setProperty("spring.liquibase.enabled", "true");
         }
         properties.setProperty("spring.liquibase.changeLog", "db/master.yaml");
         properties.setProperty("spring.liquibase.parameters.version", appVersion);
         properties.setProperty("spring.liquibase.parameters.mem", mem);
-        log.info(properties);
+        log.debug("Persistence Properties: \n" + properties);
     }
 
     private void envForJPA(Properties properties) {
