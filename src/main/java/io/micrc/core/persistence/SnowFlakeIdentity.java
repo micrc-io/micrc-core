649df57f1bfcae28196da84c6c2e20bebc656933
hyosunghan <ihyosung@qq.com>
2022-10-09 18:22:49 +0800

    hyosunghan: 1.snow flake identity generate

--- src/main/java/io/micrc/core/persistence/SnowFlakeIdentity.java
+++ src/main/java/io/micrc/core/persistence/SnowFlakeIdentity.java
@@ -2,12 +2,84 @@ package io.micrc.core.persistence;
 
 /**
  * 雪花算法ID生成器
- * TODO 在autoconfiguration中注册为bean，实例化时为其注册redistemplate用于获取服务实例id
  *
  * @author weiguan
  * @since 0.0.1
  * @date 2022-10-02 03:22
  */
 public class SnowFlakeIdentity {
-    
+
+    /**
+     * 时间戳基准值2022-01-01 00:00:00（时间戳截止值2039-01-01 00:00:00）
+     */
+    private static final long TIMESTAMP_DATUM = 1640995200000L;
+
+    /**
+     * 序列号位数
+     */
+    private static final int SEQUENCE_BITS = 9;
+
+    /**
+     * 机器码位数
+     */
+    private static final int MACHINE_NUMBER_BITS = 15;
+
+    /**
+     * 最大序列号:511(该算法可计算固定位数的二进制所能表达的最大十进制数)
+     */
+    private static final long MAX_SEQUENCE = ~(-1L << SEQUENCE_BITS);
+
+    /**
+     * 最大机器码:32767(该算法可计算固定位数的二进制所能表达的最大十进制数)
+     */
+    public static final long MAX_MACHINE_NUMBER = ~(-1L << MACHINE_NUMBER_BITS);
+
+    /**
+     * 历史时间戳，默认从当前开始，后续自增
+     */
+    private static long pastTimeStamp = System.currentTimeMillis();
+
+    /**
+     * 当前序列号，默认从0开始，后续自增
+     */
+    private static int currentSequence = 0;
+
+    /**
+     * 机器码（由业务服务初始化）
+     */
+    public static int machineNumber = -1;
+
+    /**
+     * 下一个ID
+     *
+     * @return
+     */
+    public static synchronized long nextId() {
+
+        long past = pastTimeStamp;
+        checkTime(past);
+
+        long sequence = currentSequence++;
+        if (sequence > MAX_SEQUENCE) {
+            past = ++pastTimeStamp;
+            checkTime(past);
+            currentSequence = 0;
+            sequence = currentSequence++;
+        }
+
+        return (past - TIMESTAMP_DATUM) << MACHINE_NUMBER_BITS << SEQUENCE_BITS
+                | sequence << MACHINE_NUMBER_BITS
+                | machineNumber;
+    }
+
+    /**
+     * 检查时间
+     *
+     * @param past
+     */
+    private static void checkTime(long past) {
+        while (past > System.currentTimeMillis()) {
+            // wait time go
+        }
+    }
 }
--- src/main/java/io/micrc/core/persistence/springboot/PersistenceAutoConfiguration.java
+++ src/main/java/io/micrc/core/persistence/springboot/PersistenceAutoConfiguration.java
@@ -1,6 +1,14 @@
 package io.micrc.core.persistence.springboot;
 
+import io.micrc.core.persistence.MachineNumberAliveSchedule;
+import io.micrc.core.persistence.SnowFlakeIdentity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * persistence auto configuration
@@ -10,6 +18,38 @@ import org.springframework.context.annotation.Configuration;
  * @date 2022-09-02 20:40
  */
 @Configuration
+@Import({
+        MachineNumberAliveSchedule.class
+})
 public class PersistenceAutoConfiguration {
 
+    /**
+     * 机器码键前缀
+     */
+    public static final String MACHINE_NUMBER_KEY_SUFFIX = "MACHINE:NUMBER:";
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    @Bean
+    public SnowFlakeIdentity snowFlakeIdentity() {
+        Integer number = null;
+        for (int i = 0; i <= SnowFlakeIdentity.MAX_MACHINE_NUMBER; i++) {
+            String key = MACHINE_NUMBER_KEY_SUFFIX + i;
+            String value = stringRedisTemplate.opsForValue().get(key);
+            if (value != null) {
+                continue;
+            }
+            Boolean done = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", 5, TimeUnit.MINUTES);
+            if (Boolean.TRUE.equals(done)) {
+                number = i;
+                break;
+            }
+        }
+        if (number == null) {
+            throw new IllegalStateException("There is no machine number can used.");
+        }
+        SnowFlakeIdentity.machineNumber = number;
+        return new SnowFlakeIdentity();
+    }
 }
