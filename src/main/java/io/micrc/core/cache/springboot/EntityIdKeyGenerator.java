86b6b1f99ede702fa9918069701e2a7665391430
hyosunghan <ihyosung@qq.com>
2022-11-03 11:09:14 +0800

    hyosunghan: 1.Optimized EntityIdKeyGenerator name

--- src/main/java/io/micrc/core/cache/springboot/EntityIdKeyGenerator.java
+++ src/main/java/io/micrc/core/cache/springboot/EntityIdKeyGenerator.java
@@ -14,7 +14,7 @@ import lombok.SneakyThrows;
 
 /**
  * MicrcJpaRepository接口save和saveAndFlush方法使用@CachePut时，专用的缓存key生成器
- * 使用实体id作为缓存key，实体必须有@Entity注解，且有一个名为id并注解为@Id的属性或有一个名为embeddedIdentity并注解为@EmbeddedId且有名为id的属性，这个属性值作为实体的缓存key
+ * 使用实体id作为缓存key，实体必须有@Entity注解，且有一个名为id并注解为@Id的属性或有一个名为identity并注解为@EmbeddedId且有名为id的属性，这个属性值作为实体的缓存key
  * 
  * @author weiguan
  * @since 0.0.1
@@ -34,14 +34,14 @@ public class EntityIdKeyGenerator implements KeyGenerator {
             throw new IllegalArgumentException("Caching function: " + method.getName() + "'s param must be @Entity.");
         }
         Field idField = ReflectionUtils.findField(entity.getClass(), "id");
-        Field embeddedIdentityField = ReflectionUtils.findField(entity.getClass(), "embeddedIdentity");
-        if (idField == null && embeddedIdentityField == null) {
-            throw new IllegalArgumentException("Caching function: " + method.getName() + "'s param must have a id or embeddedIdentity field");
+        Field identityField = ReflectionUtils.findField(entity.getClass(), "identity");
+        if (idField == null && identityField == null) {
+            throw new IllegalArgumentException("Caching function: " + method.getName() + "'s param must have a id or identity field");
         }
         if (idField != null) {
             return idKeyGenerator(entity, method, idField);
         } else {
-            return embeddedIdentityKeyGenerator(entity, method, embeddedIdentityField);
+            return identityKeyGenerator(entity, method, identityField);
         }
     }
 
@@ -54,23 +54,23 @@ public class EntityIdKeyGenerator implements KeyGenerator {
         return param;
     }
 
-    private Object embeddedIdentityKeyGenerator(Object target, Method method, Field embeddedIdentityField) {
-        ReflectionUtils.makeAccessible(embeddedIdentityField);
-        Object param = ReflectionUtils.getField(embeddedIdentityField, target);
-        if (param == null || embeddedIdentityField.getAnnotation(EmbeddedId.class) == null) {
+    private Object identityKeyGenerator(Object target, Method method, Field identityField) {
+        ReflectionUtils.makeAccessible(identityField);
+        Object param = ReflectionUtils.getField(identityField, target);
+        if (param == null || identityField.getAnnotation(EmbeddedId.class) == null) {
             throw new IllegalArgumentException(
                 "Caching function: " + method.getName() + "'s param must have a @EmbeddedId.");
         }
-        Field idField = ReflectionUtils.findField(embeddedIdentityField.getType(), "id");
+        Field idField = ReflectionUtils.findField(identityField.getType(), "id");
         if (idField == null) {
             throw new IllegalArgumentException(
-                "Caching function: " + method.getName() + "'s param embeddedIdentity must have a id field");
+                "Caching function: " + method.getName() + "'s param identity must have a id field");
         }
         ReflectionUtils.makeAccessible(idField);
         param = ReflectionUtils.getField(idField, param);
         if (param == null) {
             throw new IllegalArgumentException(
-                "Caching function: " + method.getName() + "'s param embeddedIdentity must have a id value.");
+                "Caching function: " + method.getName() + "'s param identity must have a id value.");
         }
         return param;
     }
