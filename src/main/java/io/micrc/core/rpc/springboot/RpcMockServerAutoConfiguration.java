4640a88610c7ef2475fcc101dc8a41bfdb38f353
teng wang <wangteng0829@gmail.com>
2022-12-28 19:25:54 +0800

    tengwang: 1.disable actuator and mockserver

--- src/main/java/io/micrc/core/rpc/springboot/RpcMockServerAutoConfiguration.java
+++ src/main/java/io/micrc/core/rpc/springboot/RpcMockServerAutoConfiguration.java
@@ -2,12 +2,9 @@ package io.micrc.core.rpc.springboot;
 
 import io.micrc.core.rpc.IntegrationsInfo;
 import org.apache.camel.component.direct.DirectComponent;
-import org.mockserver.integration.ClientAndServer;
-import org.mockserver.mock.OpenAPIExpectation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 /**
  * Mock Server
@@ -23,14 +20,14 @@ public class RpcMockServerAutoConfiguration {
         return new DirectComponent();
     }
 
-    @Profile({"default", "local"})
-    @Bean
-    public ClientAndServer clientAndServer() {
-        ClientAndServer server = ClientAndServer.startClientAndServer(1080);
-        integrationsInfo.getAll().forEach(integration -> {
-            OpenAPIExpectation openAPIExpectation = OpenAPIExpectation.openAPIExpectation(integration.getProtocolFilePath());
-            server.upsert(openAPIExpectation);
-        });
-        return server;
-    }
+//    @Profile({"default", "local"})
+//    @Bean
+//    public ClientAndServer clientAndServer() {
+//        ClientAndServer server = ClientAndServer.startClientAndServer(1080);
+//        integrationsInfo.getAll().forEach(integration -> {
+//            OpenAPIExpectation openAPIExpectation = OpenAPIExpectation.openAPIExpectation(integration.getProtocolFilePath());
+//            server.upsert(openAPIExpectation);
+//        });
+//        return server;
+//    }
 }
