9500f761251bbe3664ebf50830da2c95e9e1d8ca
hyosunghan <ihyosung@qq.com>
2024-05-13 15:46:50 +0800

    check logic status when execute runner

--- src/main/java/io/micrc/core/integration/runner/RunnerAdapterRouterExecution.java
+++ src/main/java/io/micrc/core/integration/runner/RunnerAdapterRouterExecution.java
@@ -1,6 +1,9 @@
 package io.micrc.core.integration.runner;
 
 import io.micrc.core.annotations.integration.runner.RunnerAdapter;
+import io.micrc.lib.JsonUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.CamelExecutionException;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.ProducerTemplate;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -12,6 +15,7 @@ import org.springframework.core.Ordered;
 
 import java.util.HashMap;
 
+@Slf4j
 @Aspect
 @Configuration
 public class RunnerAdapterRouterExecution implements Ordered {
@@ -33,6 +37,17 @@ public class RunnerAdapterRouterExecution implements Ordered {
         if (args.length != 1) {
             throw new IllegalArgumentException(
                     "method of application runner and annotated with RunnerAdapter only support single argument. ");
+        }
+        // check logic started
+        String status = null;
+        while (!"UP".equalsIgnoreCase(status)) {
+            try {
+                Thread.sleep(1000);
+                String string = routeTemplate.requestBody("rest://get:/q/health/live?host=localhost:8888", null, String.class);
+                status = (String) JsonUtil.readPath(string, "/status");
+            } catch (CamelExecutionException e) {
+                log.warn("waiting the logic start when execute runner: {}", interfaces[0].getSimpleName());
+            }
         }
         boolean custom = false;
         RunnerAdapter annotation = interfaces[0].getAnnotation(RunnerAdapter.class);
